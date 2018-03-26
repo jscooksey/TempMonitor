0d45ee8a6335ddd2262d33de527e15ee879a135d
Justin Cooksey <justin@cooksey.com.au>
2018-03-26 18:24:19 +1100

    First Code
    First Code

---
+++ temperatureazure.ino
@@ -0,0 +1,36 @@
+// Name:            Photon Temperature to Azure
+// Modified by:     Justin S. Cooksey
+// 20180326:        Added battery percentage in to JSON.  No data yet, waiting on sield arival
+//                  Also expecting DHT22 for better read, as well as DHT11 is providing no data at this time
+
+// This #include statement was automatically added by the Particle IDE.  
+#include <Adafruit_DHT.h>
+
+#define DHTPIN 2            // DHT11 connected to pin D2  
+#define DHTTYPE DHT11		// DHT11 currently   
+
+DHT dht(DHTPIN, DHTTYPE);  
+
+void setup()  
+{  
+    dht.begin();  
+    delay(10000);  
+}  
+void loop()  
+{  
+    char payload[62];
+    
+    //Maximum delay if using a Free (F1) Azure IoT Hub Instance (8000 request per month) that resets monthly
+    delay(652000);  
+
+    // Get data from the sensors    
+    float humidity = dht.getHumidity();  
+    float temperature = dht.getTempCelcius();  
+    float battery = 0.0; 
+     
+    // Format JSON string of data to return to Particle via publish command.  This is then hooked to Azure
+    snprintf(payload, sizeof(payload), "{\"temperature\":\"%f\",\"humidity\":\"%f\",\"battery\":\"%f\"}",temperature,humidity,battery);  
+    Particle.publish("temperature", payload);
+
+}   
+

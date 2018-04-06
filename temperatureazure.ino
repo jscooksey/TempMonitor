bbdc636c8fba62840efaa3c00f51bc4aee83c2c6
Justin Cooksey <>
2018-04-06 19:19:14 +1000

    Battery kit & DHT22
    Battery kit, batery and DHT22 have arrived.

--- temperatureazure.ino
+++ temperatureazure.ino
@@ -6,9 +6,11 @@
 // Modified by:     Justin S. Cooksey
 // 20180326:        Added battery percentage in to JSON.  No data yet, waiting on sield arival
 //                  Also expecting DHT22 for better read, as well as DHT11 is providing no data at this time
+// 20180406:        Actually have the battery unit and battery.
+//                  Also have the DHT22 to connect
 
-#define DHTPIN 2            // DHT11 connected to pin D2  
-#define DHTTYPE DHT11		// DHT11 currently   
+#define DHTPIN 2            // DHT22 connected to pin D2  
+#define DHTTYPE DHT22		// DHT22 currently   
 
 DHT dht(DHTPIN, DHTTYPE);  
 
@@ -16,12 +18,12 @@ void setup()
 {  
     dht.begin();  
     delay(10000);  
-    //lipo.begin();
-    //lipo.quickStart();
+    lipo.begin();
+    lipo.quickStart();
 }  
 void loop()  
 {  
-    char payload[62];
+    char payload[128];
     
     //Maximum delay if using a Free (F1) Azure IoT Hub Instance (8000 request per month) that resets monthly
     delay(652000);  
@@ -29,8 +31,7 @@ void loop()
     // Get data from the sensors    
     float humidity = dht.getHumidity();  
     float temperature = dht.getTempCelcius();  
-    //float battery = lipo.getSOC();
-    float battery = 95.0; 
+    float battery = lipo.getSOC();
      
     // Format JSON string of data to return to Particle via publish command.  This is then hooked to Azure
     snprintf(payload, sizeof(payload), "{\"temperature\":\"%f\",\"humidity\":\"%f\",\"battery\":\"%f\"}",temperature,humidity,battery);  
